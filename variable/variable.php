1876aef428400200ea0daa1f679007f6c72ab387
Trabelsi <trabelsi.mehdi@hotmail.com>
2017-10-31 10:43:16 +0100

    variable fini

---
+++ variable/variable.php
@@ -0,0 +1,22 @@
+<!-- Initie une variable décrivant ton prénom.
+Ensuite, sur une seconde ligne, initie une variable qui représente ton âge.
+Ensuite, à la ligne suivante, initie une variable qui décrive si tu as faim ou pas.
+Ensuite, à la ligne suivante, initie une variable qui décrive la couleur de tes yeux.
+Ensuite, à la ligne suivante, initie une variable qui décrive ta famille (avec une variable Tableau, dont chaque entrée est le prénom d'un membre de ta famille). -->
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    $prenom = Mehdi;
+    $age = 23;
+    $hungry = true;
+    $eyes_color = brun;
+    $famille = array (0 => Abdallah , 1 => Marie-louise , 2 => Mehdi);
+    ?>
+
+  </body>
+</html>
