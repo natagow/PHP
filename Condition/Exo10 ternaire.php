378b2b4fff965165b00db0741f031da54d08a69c
Trabelsi <trabelsi.mehdi@hotmail.com>
2017-11-08 11:57:10 +0100

    exo 10

---
+++ Condition/Exo10 ternaire.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>exo 10</title>
+  </head>
+  <body>
+    <?php
+    $bonjour = ($sexe == "femme")? true : false;
+    if ($bonjour == true){
+      echo "bonjour madame.";
+    } else {
+      echo "bonjour monsieur.";
+    }
+    ?>
+  </body>
+</html>
