57e0629b499068c7de39de520fb4a7852dfdce40
Trabelsi <trabelsi.mehdi@hotmail.com>
2017-11-17 16:48:21 +0100

    fonction

---
+++ Fonction/index.php
@@ -0,0 +1,17 @@
+<?php
+$name = "Maurice";
+echo "<p>Bonjour $name!</p>";
+echo '<hr>';
+
+$name = "Alice";
+echo "<p>Bonjour $name!</p>";
+echo '<hr>';
+
+$name = "Jésus";
+echo "<p>Bonjour $name!</p>";
+echo '<hr>';
+
+$name = "Judas";
+echo "<p>Bonjour $name!</p>";
+echo '<hr>';
+?>
