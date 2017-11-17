5f620586d76657daaa98fd4ce8c4fd3a044a3042
Trabelsi <trabelsi.mehdi@hotmail.com>
2017-11-17 09:33:18 +0100

    tableau finish

--- Tableau/Index.php
+++ Tableau/Index.php
@@ -1,55 +1,55 @@
 <?php
-// $pronoms_personnels = array( 'je' , 'tu' , 'il' , 'nous' , 'vous' , 'ils' );
-//   echo $pronoms_personnels[2];
-//   echo "<br>";
-//
-// $pays = array( 'Belgique', 'France' , 'Allemagne', 'Pays-Bas', 'Ukraine' );
-//   echo $pays[2];
-//   echo "<br>";
-//
-// $famille = array('Papa' , 'maman' , 'moi');
-//   print_r($famille);
-//   echo "<br>";
-//
-// $plat = array('Cheeseburger' , 'pate pesto' , 'mcdo' , 'pizza' , 'cuisinegwen');
-//   print_r($plat);
-//   echo "<br>";
-//
-// $film = array('matrix' , 'diableprada' , 'hitman' , 'inglourious basterd' );
-//   echo $film[0];
-//   echo "<br>";
-//
-// $utilisateur = array (
-//   'prenom' => 'Juan',
-//   'nom' => 'Pablo',
-//   'adresse' => '3 Rue du Paradis',
-//   'ville' => 'Bruxelles'
-//       );
-//
-// // Décris-toi dans une tableau associatif: $moi. Utilise au moins deux valeurs de chaque type:
-// //   texte (par exemple prenom), booléenne (par exemple aime_le_foot) et Int (par exemple: age).
-//
-// $moi = array (
-//   'prenom' => 'mehdi',
-//   'nom' => 'Trabelsi',
-//   'hobby' => 'cosplay',
-//   'situation' => 'en couple',
-//   'aime le foot' => false,
-//   'aime jouer' => true,
-//   'age' => 23,
-//   'durée formation' => 6
-// );
+$pronoms_personnels = array( 'je' , 'tu' , 'il' , 'nous' , 'vous' , 'ils' );
+  echo $pronoms_personnels[2];
+  echo "<br>";
 
-// $moi = array(
-// 	'naissance'	=> 1973 ,
-// 	'prenom' 		=> 'Alexandre' ,
-// 	'nom' 	  		=> 'Plennevaux' ,
-// 	'hobbies' 	=> array()
-// );
-//
+$pays = array( 'Belgique', 'France' , 'Allemagne', 'Pays-Bas', 'Ukraine' );
+  echo $pays[2];
+  echo "<br>";
 
-// print_r($moi);
-// echo '</pre>';
+$famille = array('Papa' , 'maman' , 'moi');
+  print_r($famille);
+  echo "<br>";
+
+$plat = array('Cheeseburger' , 'pate pesto' , 'mcdo' , 'pizza' , 'cuisinegwen');
+  print_r($plat);
+  echo "<br>";
+
+$film = array('matrix' , 'diableprada' , 'hitman' , 'inglourious basterd' );
+  echo $film[0];
+  echo "<br>";
+
+$utilisateur = array (
+  'prenom' => 'Juan',
+  'nom' => 'Pablo',
+  'adresse' => '3 Rue du Paradis',
+  'ville' => 'Bruxelles'
+      );
+
+// Décris-toi dans une tableau associatif: $moi. Utilise au moins deux valeurs de chaque type:
+//   texte (par exemple prenom), booléenne (par exemple aime_le_foot) et Int (par exemple: age).
+
+$moi = array (
+  'prenom' => 'mehdi',
+  'nom' => 'Trabelsi',
+  'hobby' => 'cosplay',
+  'situation' => 'en couple',
+  'aime le foot' => false,
+  'aime jouer' => true,
+  'age' => 23,
+  'durée formation' => 6
+);
+
+$moi = array(
+	'naissance'	=> 1973 ,
+	'prenom' 		=> 'Alexandre' ,
+	'nom' 	  		=> 'Plennevaux' ,
+	'hobbies' 	=> array()
+);
+
+
+print_r($moi);
+echo '</pre>';
 
 
 
@@ -119,6 +119,66 @@ $moi = array (
          $nos_hobbies= $hobbies_moi + $hobbies_papa;
          echo $nos_hobbies;
 
-array_push($moi['hobbies'],'taxidermie');
+         array_push($moi['hobbies'],'taxidermie');
+?>
 
-       ?>
+
+<?php
+      $moi = array(
+        'prenom' => 'Mehdi',
+        'hobbies' => array (
+                              'sport' => 'fresbee',
+                              'jeu'=> 'overwatch',
+                              'artistique'=>'cosplay'
+                            ),
+      );
+      $ame_soeur = array(
+        'prenom' => 'Gwen',
+        'hobbies' => array (
+                              'creation' => 'bijou',
+                              'jeu'=> 'overwatch',
+                              'artistique'=>'graphiste',
+                            ),
+      );
+        echo '<pre>';
+          print_r($moi);
+        echo '</pre>';
+        echo '<pre>';
+          print_r($ame_soeur);
+        echo '</pre>';
+        echo "Les hobbies de notre enfant si intersection : <br/>";
+        $hobbie_enfant_inter = array_intersect ($moi['hobbies'], $ame_soeur['hobbies']);
+        print_r ($hobbie_enfant_inter);
+        echo '<br/>';
+        echo "Les hobbies de notre enfant si fusion : <br/>";
+        $hobbie_enfant_fusion = array_merge($moi['hobbies'], $ame_soeur['hobbies']);
+        print_r ($hobbie_enfant_fusion);
+      ?>
+
+      <?php
+      $web_development = array (
+          'frontend' => array(
+          ),
+          'backend' => array(
+          ),
+        );
+      echo '<pre>';
+        print_r($web_development);
+      echo '</pre>';
+      array_push ($web_development[frontend],'xhtml');
+      array_push ($web_development[backend],'Ruby on Rails');
+      array_push ($web_development[frontend],'CSS');
+      array_push ($web_development[frontend],'Flash');
+      array_push ($web_development[frontend],'Javascript');
+      echo '<pre>';
+        print_r($web_development);
+      echo '</pre>';
+      $web_development[frontend][0] ='HTML';
+      echo '<pre>';
+        print_r($web_development);
+      echo '</pre>';
+      unset($web_development[frontend][2]);
+      echo '<pre>';
+        print_r($web_development);
+      echo '</pre>';
+            ?>
